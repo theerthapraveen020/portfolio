07df471ae7dcc4ad821adf7e54a33852e8846c99
TheerthaPraveen9817 <theerthapraveen026@gmail.com>
2026-01-21 13:45:26 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+/*=============== SHOW & CLOSE MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Show menu */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Hide menu */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+/*=============== REMOVE MOBILE MENU ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+   const navMenu = document.getElementById('nav-menu')
+   // When we click on each nav__link, we remove the show-menu class
+   navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+{/* home typed js*/}
+ const typedHome = new Typed('#home-typed', {
+      strings: ['Developer' ,'Full stack Developer'],
+      typeSpeed: 80,
+      backSpeed:40,
+      backDelay:2000,
+      loop:true,
+      cursorChar:'_',
+    });
+
+/*=============== CHANGE HEADER STYLES ===============*/
+const shadowHeader = () =>{
+   const header = document.getElementById('header')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+   this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                      : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
